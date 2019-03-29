2feb2c466ad1b9741455f3797f57e5c882bc0911
demel42 <demel42@web.de>
2019-03-29 16:35:39 +0100

    SetValue() abgesichert

--- libs/common.php
+++ libs/common.php
@@ -24,7 +24,7 @@ trait SyslogCommon
             return;
         }
 
-        $ret = parent::SetValue($Ident, $Value);
+        @$ret = parent::SetValue($Ident, $Value);
         if ($ret == false) {
             $this->SendDebug(__FUNCTION__, 'mismatch of value "' . $Value . '" for variable ' . $Ident, 0);
         }
